7890f99840875d96568bacb07b50644eb9dccc49
mdheller <mdheller@gmail.com>
2022-02-26 17:15:24 -0500

    Update socios.rb

--- socios.rb
+++ socios.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula -Socios"
-  homepage "https://github.com/SociOS-Linux/socios"
-  url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.2.3.tar.gz"
+  homepage "https://github.com/Bsivakumar29/socios"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.3.tar.gz"
   sha256 "e48c21a70b3c8fa2cf9e17786475272668c6d17b7c8e41b59f6d233c6be6e488"
   license ""
 
