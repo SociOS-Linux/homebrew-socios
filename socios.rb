763e46c237ba6246b05b5e267066019d2a61b3a2
mdheller <mdheller@gmail.com>
2022-03-04 22:17:23 -0500

    Update socios.rb

--- socios.rb
+++ socios.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class socios < Formula
   desc "development formula -socios"
-  homepage "https://github.com/Bsivakumar29/socios-setup"
+  homepage "https://github.com/Bsivakumar29/socios"
   url "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/socios.rb"
   sha256 "b961c646165d69926cbcea897f0d8ed335aba6a99d45fed7667f41196a00ef3e"
   license "GPL-3.0"
