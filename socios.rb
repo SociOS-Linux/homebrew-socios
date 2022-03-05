f0b2a78ef0b06e33f0fd3c2257140f6b7f63dc8c
mdheller <mdheller@gmail.com>
2022-03-04 22:17:57 -0500

    Update socios.rb

--- socios.rb
+++ socios.rb
@@ -1,9 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class socios < Formula
-  desc "development formula -socios"
-  homepage "https://github.com/Bsivakumar29/socios"
+class Socios < Formula
+  desc "development formula -Socios"
+  homepage "https://github.com/SociOS-Linux/socios"
   url "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/socios.rb"
   sha256 "b961c646165d69926cbcea897f0d8ed335aba6a99d45fed7667f41196a00ef3e"
   license "GPL-3.0"
