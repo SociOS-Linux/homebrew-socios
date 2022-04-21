3a8ddf5284593432d9e10a0157aaebfa3a4fb05e
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-04-21 21:12:01 +0530

    Socios-setup: Updated SHA key in Home-brew formula

--- socios.rb
+++ socios.rb
@@ -5,7 +5,7 @@ class Socios < Formula
   desc "development formula -Socios"
   homepage "https://github.com/SociOS-Linux/socios"
   url "https://github.com/SociOS-Linux/socios/releases/download/v1.2.3.alpha/socios-setup.tar.gz"
-  sha256 "b961c646165d69926cbcea897f0d8ed335aba6a99d45fed7667f41196a00ef3e"
+  sha256 "a465f38992f66bfa4f855c2f834fe0e55d5a109d0b3123c50701c9c341962cf0"
   license "GPL-3.0"
 
   def install
