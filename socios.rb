f1ec3256a0f855cf5868ecfc38a890f189dc1210
Anilkumar18 <anikumarunni@gmail.com>
2022-05-18 20:33:08 +0530

    Socios: Updated version V1.3.5

--- socios.rb
+++ socios.rb
@@ -3,9 +3,9 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula -Socios"
-  homepage "https://github.com/SociOS-Linux/socios" 
-  url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.3.4.tar.gz"
-  sha256 "00841092ffec9fc01a3f66fc6c17f1f6f6a04e05d6d4ea86d61e22654020a798"
+  homepage "https://github.com/SociOS-Linux/socios"
+  url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.3.5.tar.gz"
+  sha256 "d653dcf8c4ee27906e511a8290bc1c7c3ae96362c19dfc0ee22839a1276c3958"
   license "GPL-3.0"
 
   # depends_on "cmake" => :build
