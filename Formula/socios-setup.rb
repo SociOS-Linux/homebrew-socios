953536236fb2157e243eb234f5c2bf47b36fdba2
mdheller <mdheller@gmail.com>
2022-05-30 17:29:10 -0400

    Update and rename socios.rb to socios-setup.rb
    renamed to socios-setup where appropriate.

--- Formula/socios-setup.rb
+++ Formula/socios-setup.rb
@@ -3,15 +3,15 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula -Socios"
-  homepage "https://github.com/SociOS-Linux/socios-installer"
-  url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.4.0.tar.gz"
+  homepage "https://github.com/SociOS-Linux/socios-setup"
+  url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.0.tar.gz"
   sha256 "2034fa849aad5d93e179e26f8b7d2bedc3f5c0a675a48b4ece9afcc5acad5b38"
   license "GPL-3.0"
 
   # depends_on "cmake" => :build
 
   def install
-	bin.install "socios"
+	bin.install "socios-setup"
 	prefix.install Dir["lib"]
   end
 end
