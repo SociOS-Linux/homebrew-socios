b6c4384eaa5f567be681e9c2005fff2169adae72
mdheller <mdheller@gmail.com>
2022-05-30 20:49:31 -0400

    Update socios-setup.rb

--- Formula/socios-setup.rb
+++ Formula/socios-setup.rb
@@ -1,8 +1,8 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Socios < Formula
-  desc "development formula -Socios"
+class SociosSetup < Formula
+  desc "development formula -SociosSetup"
   homepage "https://github.com/SociOS-Linux/socios-setup"
   url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.0.tar.gz"
   sha256 "2034fa849aad5d93e179e26f8b7d2bedc3f5c0a675a48b4ece9afcc5acad5b38"
