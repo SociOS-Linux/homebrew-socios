e44575972d8cf589226a1f35e4bbc843cb719a5c
Anilkumar18 <anikumarunni@gmail.com>
2022-06-01 20:17:19 +0530

    “Socios:UpdatedversionV1.4.4”

--- socios-setup.rb
+++ socios-setup.rb
@@ -2,14 +2,16 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SociosSetup < Formula
-  desc "Development formula SociosSetup"
-  homepage "https://github.com/SociOS-Linux/socios-setup"
-  url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "1a0a12acffc5411e6baee5521c2ccf216d120ab373015886cef2ba9ef143f238"
+  desc "Development formula -Socios"
+  homepage ""
+  url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.4.tar.gz"
+  sha256 "66e564577fd5c5f48d4b356360c41d7d5d4a89410c23b8f1b127ada64ef2a651"
   license "GPL-3.0"
 
+  # depends_on "cmake" => :build
+
   def install
-	bin.install "socios-setup"
-	prefix.install Dir["lib"]
-  end
+	bin.install "socios"
+        prefix.install Dir["lib"]
+     end
 end
