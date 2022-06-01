272712e6cb77a92ad2dc1a46719982bff22f1dd7
Anilkumar18 <anikumarunni@gmail.com>
2022-06-01 14:08:32 +0530

    Socios version V1.4.2

--- Formula/socios-setup.rb
+++ Formula/socios-setup.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SociosSetup < Formula
-  desc "Development formula -SociosSetup"
+  desc "Development formula SociosSetup"
   homepage "https://github.com/SociOS-Linux/socios-setup"
   url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.2.tar.gz"
   sha256 "1a0a12acffc5411e6baee5521c2ccf216d120ab373015886cef2ba9ef143f238"
