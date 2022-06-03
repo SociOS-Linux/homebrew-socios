418b3c3705b9a87f7d3ea0690fc3cc0189a45bea
mdheller <mdheller@gmail.com>
2022-06-03 14:25:58 -0400

    Update and rename socios-setup.rb to socios.rb

--- socios.rb
+++ socios.rb
@@ -1,10 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class SociosSetup < Formula
+class Socios < Formula
   desc "Development formula -Socios"
   homepage ""
-  url "https://github.com/SociOS-Linux/socios-setup/archive/refs/tags/v1.4.4.tar.gz"
+  url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.4.4.tar.gz"
   sha256 "66e564577fd5c5f48d4b356360c41d7d5d4a89410c23b8f1b127ada64ef2a651"
   license "GPL-3.0"
 
