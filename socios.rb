f7831e0e78ce51e05f866aed7507b8398ecfd4d7
mdheller <mheller@us.ibm.com>
2022-06-04 14:58:41 -0400

    socios: committing this now for v1.4.7

--- socios.rb
+++ socios.rb
@@ -2,16 +2,32 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
-  desc "socios seup installer"
-  homepage "https://github.com/SociOS-Linux/homebrew-socios"
+  desc ""
+  homepage ""
   url "https://github.com/SociOS-Linux/socios/archive/refs/tags/v1.4.7.tar.gz"
-  sha256 "56d83d661519e3b5420887f784dd55100ae8c3e178dc8a1c8c2dc247ce3b785c"
-  license "MIT"
+  sha256 "5cfd5654e77f9bf949819bef7814d72511eb2066d9971db3a0a60a94b06f51be"
+  license "GPL-3.0"
 
   # depends_on "cmake" => :build
 
   def install
-              bin.install "socios"
-              prefix.install Dir["lib"]
-     end
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test socios`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
